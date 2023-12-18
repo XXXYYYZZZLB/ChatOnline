316fe959d299699e96cf0e01d44b12a1f435b690
libin <libinnn2020@163.com>
2023-12-18 16:24:58 +0800

    后端阶段性完成

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/mycode/chat/src/server/chatserver.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/root/mycode/chat/src/server/chatservice.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/root/mycode/chat/src/server/db/db.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/root/mycode/chat/src/server/main.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/root/mycode/chat/src/server/model/friendmoel.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/model/friendmoel.cpp.o"
+  "/root/mycode/chat/src/server/model/groupmodel.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/root/mycode/chat/src/server/model/offlinemessagemodel.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/root/mycode/chat/src/server/model/usermodel.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/root/mycode/chat/src/server/redis/redis.cpp" "/root/mycode/chat/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
